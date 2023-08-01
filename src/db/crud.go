cd6332319ec31b746261a1777ea4e93ac5be190e
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2023-08-01 22:06:49 +0530

    refactor: add CRUD functions

---
+++ src/db/crud.go
@@ -0,0 +1,84 @@
+package db
+
+import (
+	"context"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+func InsertUserNodeToDB(ctx context.Context, driver neo4j.DriverWithContext, fid int, username string) error {
+	_, err := neo4j.ExecuteQuery(
+		ctx,
+		driver,
+		"MERGE (u:User {fid: $fid, username: $username})",
+		map[string]any{"fid": fid, "username": username},
+		neo4j.EagerResultTransformer,
+	)
+	return err
+}
+
+func InsertPostNodeToDB(ctx context.Context, driver neo4j.DriverWithContext, hash string) error {
+	_, err := neo4j.ExecuteQuery(
+		ctx,
+		driver,
+		"MERGE (c:Cast {hash: $hash})",
+		map[string]any{"hash": hash},
+		neo4j.EagerResultTransformer,
+	)
+	return err
+}
+
+func CreateFollowsEdge(ctx context.Context, driver neo4j.DriverWithContext, fid1 int, fid2 int) error {
+	_, err := neo4j.ExecuteQuery(
+		ctx,
+		driver,
+		"MATCH (u1:User {fid: $fid1}), (u2:User {fid: $fid2}) MERGE (u1)-[r:FOLLOWS]->(u2)",
+		map[string]any{"fid1": fid1, "fid2": fid2},
+		neo4j.EagerResultTransformer,
+	)
+	return err
+}
+
+func CreatePublishedEdge(ctx context.Context, driver neo4j.DriverWithContext, fid int, hash string) error {
+	_, err := neo4j.ExecuteQuery(
+		ctx,
+		driver,
+		"MATCH (c:Cast {hash: $hash}), (u:User {fid: $fid}) MERGE (u)-[r:PUBLISHED]->(c)",
+		map[string]any{"hash": hash, "fid": fid},
+		neo4j.EagerResultTransformer,
+	)
+	return err
+}
+
+func CreateLikedEdge(ctx context.Context, driver neo4j.DriverWithContext, fid int, hash string) error {
+	_, err := neo4j.ExecuteQuery(
+		ctx,
+		driver,
+		"MATCH (c:Cast {hash: $hash}), (u:User {fid: $fid}) MERGE (u)-[r:LIKED]->(c)",
+		map[string]any{"hash": hash, "fid": fid},
+		neo4j.EagerResultTransformer,
+	)
+	return err
+}
+
+func CreateRecastedEdge(ctx context.Context, driver neo4j.DriverWithContext, fid int, hash string) error {
+	_, err := neo4j.ExecuteQuery(
+		ctx,
+		driver,
+		"MATCH (c:Cast {hash: $hash}), (u:User {fid: $fid}) MERGE (u)-[r:RECASTED]-(c)",
+		map[string]any{"hash": hash, "fid": fid},
+		neo4j.EagerResultTransformer,
+	)
+	return err
+}
+
+func CreateChildOfEdge(ctx context.Context, driver neo4j.DriverWithContext, parentHash string, childHash string) error {
+	_, err := neo4j.ExecuteQuery(
+		ctx,
+		driver,
+		"MATCH (pc:Cast {hash: $parentHash}), (cc:Cast {hash: $childHash}) MERGE (cc)-[r:CHILD_OF]-(pc)",
+		map[string]any{"parentHash": parentHash, "childHash": childHash},
+		neo4j.EagerResultTransformer,
+	)
+	return err
+}
