193f1eb016fe3746e9aeaf3c81d60fb4fffb885c
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2023-08-05 23:33:45 +0530

    refactor: change package layout

--- main.go
+++ main.go
@@ -6,123 +6,12 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ashutosh1206/fargraph/internal/db"
+	"github.com/ashutosh1206/fargraph/internal/fargraph"
 	"github.com/ashutosh1206/fargraph/pkg/farcaster"
-	"github.com/ashutosh1206/fargraph/src/db"
 	"github.com/joho/godotenv"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
-
-func retrieveUserFollowers(
-	ctx context.Context,
-	driver neo4j.DriverWithContext,
-	fid int,
-	pageLimit int,
-	fcClient *farcaster.FCRequestClient,
-) error {
-	followersPaginated, cursor, err := fcClient.GetFollowersPaginated(fid, "", pageLimit)
-	if err != nil {
-		return err
-	}
-	err = db.InsertFollowersToDB(ctx, driver, followersPaginated, fid)
-	if err != nil {
-		return err
-	}
-	for cursor != "" {
-		followersPaginated, cursor, err = fcClient.GetFollowersPaginated(fid, cursor, pageLimit)
-		if err != nil {
-			return err
-		}
-		err = db.InsertFollowersToDB(ctx, driver, followersPaginated, fid)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func retrieveUserFollowing(
-	ctx context.Context,
-	driver neo4j.DriverWithContext,
-	fid int,
-	pageLimit int,
-	fcClient *farcaster.FCRequestClient,
-) error {
-	followingPaginated, cursor, err := fcClient.GetFollowingPaginated(fid, "", pageLimit)
-	if err != nil {
-		return err
-	}
-	err = db.InsertFollowingToDB(ctx, driver, followingPaginated, fid)
-	if err != nil {
-		return err
-	}
-	for cursor != "" {
-		followingPaginated, cursor, err = fcClient.GetFollowingPaginated(fid, cursor, pageLimit)
-		if err != nil {
-			return err
-		}
-		err = db.InsertFollowingToDB(ctx, driver, followingPaginated, fid)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func retrieveUserLikedCasts(
-	ctx context.Context,
-	driver neo4j.DriverWithContext,
-	fid int,
-	pageLimit int,
-	fcClient *farcaster.FCRequestClient,
-) error {
-	likedCastsPaginated, cursor, err := fcClient.GetUserLikedPaginated(fid, "", pageLimit)
-	if err != nil {
-		return err
-	}
-	err = db.InsertUserLikesToDB(ctx, driver, likedCastsPaginated, fid)
-	if err != nil {
-		return err
-	}
-	for cursor != "" {
-		likedCastsPaginated, cursor, err = fcClient.GetUserLikedPaginated(fid, cursor, pageLimit)
-		if err != nil {
-			return err
-		}
-		err = db.InsertUserLikesToDB(ctx, driver, likedCastsPaginated, fid)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func retrieveUserCasts(
-	ctx context.Context,
-	driver neo4j.DriverWithContext,
-	fid int,
-	pageLimit int,
-	fcClient *farcaster.FCRequestClient,
-) error {
-	userCastsPaginated, cursor, err := fcClient.GetUserCastsPaginated(fid, "", pageLimit)
-	if err != nil {
-		return err
-	}
-	err = db.InsertUserPostsToDB(ctx, driver, userCastsPaginated, fid)
-	if err != nil {
-		return err
-	}
-	for cursor != "" {
-		userCastsPaginated, cursor, err = fcClient.GetUserCastsPaginated(fid, cursor, pageLimit)
-		if err != nil {
-			return err
-		}
-		err = db.InsertUserPostsToDB(ctx, driver, userCastsPaginated, fid)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
 
 func main() {
 	// Load .env
@@ -175,28 +64,28 @@ func main() {
 	}
 
 	fmt.Println("Getting followers")
-	err = retrieveUserFollowers(ctx, driver, fid, pageLimit, fcRequestClient)
+	err = fargraph.RetrieveUserFollowers(ctx, driver, fid, pageLimit, fcRequestClient)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Inserted followers")
 
 	fmt.Println("Getting following")
-	err = retrieveUserFollowing(ctx, driver, fid, pageLimit, fcRequestClient)
+	err = fargraph.RetrieveUserFollowing(ctx, driver, fid, pageLimit, fcRequestClient)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Inserted following")
 
 	fmt.Println("Getting list of posts liked by user")
-	err = retrieveUserLikedCasts(ctx, driver, fid, pageLimit, fcRequestClient)
+	err = fargraph.RetrieveUserLikedCasts(ctx, driver, fid, pageLimit, fcRequestClient)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Inserted liked posts")
 
 	fmt.Println("Getting user casts, recasts and replies")
-	err = retrieveUserCasts(ctx, driver, fid, pageLimit, fcRequestClient)
+	err = fargraph.RetrieveUserCasts(ctx, driver, fid, pageLimit, fcRequestClient)
 	if err != nil {
 		panic(err)
 	}
