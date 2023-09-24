90a8dee4c2f95e43300f971d0e83d57d808e22b8
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2023-09-25 00:30:42 +0530

    feat: get user info

--- pkg/farcaster/user.go
+++ pkg/farcaster/user.go
@@ -2,12 +2,14 @@ package farcaster
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/url"
 )
 
 type userResponse struct {
 	Result struct {
-		User User
+		User    User `json:"user"`
+		Inviter User `json:"inviter"`
 	} `json:"result"`
 }
 
@@ -18,6 +20,68 @@ func (client *FCRequestClient) GetUserInfoByUsername(username string) (User, err
 	query.Add("username", username)
 
 	requestUrl, err := url.JoinPath(client.BaseUrl, "/v2/user-by-username")
+	if err != nil {
+		return userInfo, err
+	}
+
+	respBody, err := makeWarpcastRequest(
+		requestUrl,
+		query,
+		client.appBearerToken,
+		client.HTTPClient,
+	)
+	if err != nil {
+		return userInfo, err
+	}
+
+	// Parsing the response
+	var responseStruct userResponse
+	err = json.Unmarshal(respBody, &responseStruct)
+	if err != nil {
+		return userInfo, err
+	}
+	userInfo = responseStruct.Result.User
+
+	return userInfo, nil
+}
+
+func (client *FCRequestClient) GetRecentUsersPaginated(cursor string, limit int) ([]User, string, error) {
+	users := make([]User, 0, limit)
+
+	query := make(url.Values, 2)
+	query.Add("limit", fmt.Sprint(limit))
+	if cursor != "" {
+		query.Add("cursor", cursor)
+	}
+
+	requestUrl, err := url.JoinPath(client.BaseUrl, "/v2/user-liked-casts")
+	if err != nil {
+		return users, "", err
+	}
+
+	respBody, err := makeWarpcastRequest(requestUrl, query, client.appBearerToken, client.HTTPClient)
+	if err != nil {
+		return users, "", err
+	}
+
+	// Parsing the response
+	var responseStruct usersResponse
+	err = json.Unmarshal(respBody, &responseStruct)
+	if err != nil {
+		return users, "", err
+	}
+	users = responseStruct.Result.Users
+
+	return users, responseStruct.Next.Cursor, nil
+}
+
+func (client *FCRequestClient) GetUserByFid(fid int) (User, error) {
+	var userInfo User
+
+	query := make(url.Values, 1)
+	query.Add("fid", fmt.Sprint(fid))
+
+	requestUrl, err := url.JoinPath(client.BaseUrl, "/v2/user")
 	if err != nil {
 		return userInfo, err
 	}
